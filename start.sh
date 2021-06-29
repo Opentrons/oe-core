fcd535d7decbaf1abd3649eef60985736ff0b9a4
Seth Foster <seth@opentrons.com>
2021-06-29 09:44:55 -0400

    fixups

--- start.sh
+++ start.sh
@@ -8,11 +8,8 @@ trap cleanup EXIT
 
 pushd $(dirname ${0})
 
-rm -f ./build/tmp-glibc
-rm -rf ./build/deploy
 source ./export
 patch ./layers/meta-toradex-nxp/recipes-kernel/linux/linux-toradex_5.4-2.3.x.bb ./linux-toradex_5.4-2.3.x.patch
 
 cd build
-BB_NUMBER_THREADS=$((`nproc`-1)) bitbake -c cleanall tdx-reference-minimal-image
-BB_NUMBER_THREADS=$((`nproc`-1)) bitbake tdx-reference-minimal-image
+BB_NUMBER_THREADS=$((`nproc`-1)) bitbake -c tdx-reference-minimal-image
