af7990fe7824b54b35ad5f7f1e30126496ad50e0
Seth Foster <seth@opentrons.com>
2021-06-30 17:08:24 -0400

    try normal export

--- start.sh
+++ start.sh
@@ -14,7 +14,7 @@ pushd ${THISDIR}
 patch ./layers/meta-toradex-nxp/recipes-kernel/linux/linux-toradex_5.4-2.3.x.bb ./linux-toradex_5.4-2.3.x.patch
 
 export BITBAKEDIR=${THISDIR}/tools/bitbake
-. ./layers/openembedded-core/oe-init-build-env ./build
+. export
 
 ls -la .
 rm -rf ./build/tmp/*
