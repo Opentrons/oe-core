4601e6d12c90cae9aaddf932efbc9409be2a2db6
Seth Foster <seth@opentrons.com>
2021-06-30 14:23:31 -0400

    correct toradex-demos, quiet popd

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
 cleanup () {
-  popd
+  popd >/dev/null
 }
 
 trap cleanup EXIT
@@ -12,7 +12,6 @@ pushd ${THISDIR}
 patch ./layers/meta-toradex-nxp/recipes-kernel/linux/linux-toradex_5.4-2.3.x.bb ./linux-toradex_5.4-2.3.x.patch
 
 export BITBAKEDIR=${THISDIR}/tools/bitbake
-
 . ./layers/openembedded-core/oe-init-build-env ./build
 
 BB_NUMBER_THREADS=$((`nproc`-1)) bitbake -c tdx-reference-minimal-image
