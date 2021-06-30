ed750a807e426e7397aee15cb174a813a69f5add
Seth Foster <seth@opentrons.com>
2021-06-30 15:05:22 -0400

    manually mak a tmp

--- start.sh
+++ start.sh
@@ -9,6 +9,8 @@ trap cleanup EXIT
 THISDIR=$(dirname ${0})
 pushd ${THISDIR}
 
+mkdir -p ./build/tmp
+
 patch ./layers/meta-toradex-nxp/recipes-kernel/linux/linux-toradex_5.4-2.3.x.bb ./linux-toradex_5.4-2.3.x.patch
 
 export BITBAKEDIR=${THISDIR}/tools/bitbake
