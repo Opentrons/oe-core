f9c08819c26ad5a67423f1f58fe181e6b4c90d17
Seth Foster <seth@opentrons.com>
2021-07-08 16:08:00 -0400

    update branches to whatever github says

--- update.sh
+++ update.sh
@@ -4,4 +4,8 @@ set -euo pipefail
 # Update all the submodules with their specific needs.
 
 git submodule update --checkout --init
+if [ -z "${GITHUB_REF+x}" ]; then
+   echo "CI variables detected, setting opentrons-controlled repos as specified"
+   git submodule set-branch -b ${GITHUB_REF#/refs/heads} layers/meta-opentrons
+fi
 git submodule update --checkout --remote layers/meta-opentrons
