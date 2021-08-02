67ed3b2158da7b906590b64c5b001ad111ac6b5d
Seth Foster <seth@opentrons.com>
2021-08-02 13:52:21 -0400

    chore(build): use opetnrons distro
    And tweak some various configurations around
    
    Our distro is derived from the upstream toradex distro with some config
    changes to allow for better recipe overrides and configuration through
    preverred version.
    
    We also now use a local fork for meta-freescale. Until we resolve
    https://github.com/Freescale/meta-freescale/pull/841 we should use a
    fork at https://github.com/Opentrons/meta-freescale that fixes the issue.

--- ot3Image.sh
+++ ot3Image.sh
@@ -5,8 +5,9 @@ if [ -z ${BACKGROUND} ]; then
 else
     RUNSTYLE="-d"
 fi
+
 HEREPATH=$(dirname $(realpath "${0}"))
 tmp_dir=$(mktemp -d -t ci-XXXXXXX)
 cp "${HEREPATH}/start.sh" ${tmp_dir}/
 docker build -f "${HEREPATH}/Dockerfile" --build-arg "username=`whoami`" --build-arg "host_uid=`id -u`"   --build-arg "host_gid=`id -g`" --tag "ot3-image:latest" ${tmp_dir}
-docker run ${RUNSTYLE} --rm --mount type=bind,src="${HEREPATH}",dst=/volumes/oe-core,consistency=delegated ot3-image:latest "$@"
+docker run ${RUNSTYLE} ${EP} --rm --mount type=bind,src="${HEREPATH}",dst=/volumes/oe-core,consistency=delegated ot3-image:latest "$@"
