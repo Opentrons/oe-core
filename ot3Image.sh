dfe4f86f71cc71ecbc4adae2d0dd203c2b0c80de
Seth Foster <seth@opentrons.com>
2022-11-10 14:09:46 -0500

    mount monorepo side by side with oe-core

--- ot3Image.sh
+++ ot3Image.sh
@@ -1,12 +1,16 @@
 #! /bin/bash
 
-if [ -z ${BACKGROUND} ]; then
+if [ -z "${BACKGROUND}" ]; then
     RUNSTYLE="-it"
 else
     RUNSTYLE="-d"
 fi
-HEREPATH=$(dirname $(realpath "${0}"))
+HEREPATH=$(dirname "$(realpath "${0}")")
 tmp_dir=$(mktemp -d -t ci-XXXXXXX)
-cp "${HEREPATH}/start.sh" ${tmp_dir}/
+cp "${HEREPATH}/start.sh" "${tmp_dir}"/
 docker build -f "${HEREPATH}/Dockerfile" --build-arg "username=`whoami`" --build-arg "host_uid=`id -u`"   --build-arg "host_gid=`id -g`" --tag "ot3-image:latest" ${tmp_dir}
-docker run ${RUNSTYLE} --rm --mount type=bind,src="${HEREPATH}",dst=/volumes/oe-core,consistency=delegated ot3-image:latest "$@"
+
+MONOREPO_PATH=$(realpath "$HEREPATH"/../opentrons)
+OE_MOUNT="type=bind,src=$HEREPATH,dst=/volumes/oe-core,consistency=delegated"
+MONOREPO_MOUNT="type=bind,src=$MONOREPO_PATH,dst=/volumes/opentrons,consistency=delegated"
+docker run ${RUNSTYLE} --rm --mount "$OE_MOUNT" --mount "$MONOREPO_MOUNT"  -- ot3-image:latest "$@"
