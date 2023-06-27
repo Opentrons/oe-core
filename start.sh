76dbf27bce3948ab9fad2882eb2ce08ae790752a
Brayan Almonte <vegano1@users.noreply.github.com>
2023-06-27 14:31:24 -0400

    fix(build): create symlink for electron cache dir manually (#84)

--- start.sh
+++ start.sh
@@ -33,5 +33,11 @@ patch -f ./layers/meta-jupyter/conf/layer.conf ./meta-jupyter-backport.patch
 export BITBAKEDIR=${THISDIR}/tools/bitbake
 . layers/openembedded-core/oe-init-build-env ${THISDIR}/build
 
+# electron is ignoring the cache download set by the electron_config_cache env var
+# so for now lets manually create a symlink and set its download location to /volumes/cache
+mkdir -p /volumes/cache/electron
+mkdir -p ~/.cache/
+ln -sf /volumes/cache/electron ~/.cache/electron
+
 BB_NUMBER_THREADS=$(nproc) bitbake ${TARGET} "$@"
 exit $?
