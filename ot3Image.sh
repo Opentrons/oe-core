fd287b0a2411ef8e10e39dce997c4cf2ce6fda08
vegano1 <brayan.almonte@gmail.com>
2023-07-21 12:00:08 -0400

    wip

--- ot3Image.sh
+++ ot3Image.sh
@@ -1,5 +1,8 @@
 #! /bin/bash
 
+# need to use docker buildkit to safely pass in secret keys
+export DOCKER_BUILDKIT=1
+
 if [ -z "${BACKGROUND}" ]; then
     RUNSTYLE="-it"
 else
@@ -8,11 +11,11 @@ fi
 HEREPATH=$(dirname "$(realpath "${0}")")
 tmp_dir=$(mktemp -d -t ci-XXXXXXX)
 cp "${HEREPATH}/start.sh" "${tmp_dir}"/
-docker build -f "${HEREPATH}/Dockerfile" --build-arg "username=`whoami`" --build-arg "host_uid=`id -u`"   --build-arg "host_gid=`id -g`" --tag "ot3-image:latest" ${tmp_dir}
+docker build -f "${HEREPATH}/Dockerfile" --secret id=SECRET_KEY,src=secret-file --build-arg "username=`whoami`" --build-arg "host_uid=`id -u`" --build-arg "host_gid=`id -g`" --tag "ot3-image:latest" ${tmp_dir}
 
 MONOREPO_PATH=$(realpath "$HEREPATH"/../opentrons)
 OT3_FW_PATH=$(realpath "$HEREPATH"/../ot3-firmware)
 OE_MOUNT="type=bind,src=$HEREPATH,dst=/volumes/oe-core,consistency=delegated"
 MONOREPO_MOUNT="type=bind,src=$MONOREPO_PATH,dst=/volumes/opentrons,consistency=delegated"
 OT3_FW_MOUNT="type=bind,src=$OT3_FW_PATH,dst=/volumes/ot3-firmware,consistency=delegated"
-docker run ${RUNSTYLE} --rm --mount "$OE_MOUNT" --mount "$MONOREPO_MOUNT"  --mount "$OT3_FW_MOUNT" -- ot3-image:latest "$@"
+docker run ${RUNSTYLE} --mount=type=secret,id=SECRET_KEY --rm --mount "$OE_MOUNT" --mount "$MONOREPO_MOUNT" --mount "$OT3_FW_MOUNT" -- ot3-image:latest "$@"
